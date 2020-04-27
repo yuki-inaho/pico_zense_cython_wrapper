cc414a9d2cba42bb42566f94ff9e745d1adff8c1
yuki-inaho <yoshikawa@inaho.co>
2020-04-27 22:43:21 +0900

    Code arrangement

--- main.py
+++ main.py
@@ -20,18 +20,11 @@ def main(out):
     dict_toml["Camera0"]["serial_no"] = zense.getSerialNumber()
     params = zense.getCameraParameter()
 
-    dict_toml["Camera0_Factory"]["fx"] = params[0]
-    dict_toml["Camera0_Factory"]["fy"] = params[1]
-    dict_toml["Camera0_Factory"]["cx"] = params[2]
-    dict_toml["Camera0_Factory"]["cy"] = params[3]
-    dict_toml["Camera0_Factory"]["p1"] = params[4]
-    dict_toml["Camera0_Factory"]["p2"] = params[5]
-    dict_toml["Camera0_Factory"]["k1"] = params[6]
-    dict_toml["Camera0_Factory"]["k2"] = params[7]
-    dict_toml["Camera0_Factory"]["k3"] = params[8]
-    dict_toml["Camera0_Factory"]["k4"] = params[9]
-    dict_toml["Camera0_Factory"]["k5"] = params[10]
-    dict_toml["Camera0_Factory"]["k6"] = params[11]
+    intrinsic_elems = ["fx", "fy", "cx", "cy", \
+                       "p1", "p2", "k1", "k2", \
+                       "k3", "k4", "k5", "k6"]
+    for i, _elem in enumerate(intrinsic_elems):
+        dict_toml["Camera0_Factory"][_elem] = params[i]
 
     with open(out, "w") as f:
         toml.encoder.dump(dict_toml, f)
