fe55340358f8fb591a07a09264c36087ec7194bf
yuki-inaho <yoshikawa@inaho.co>
2020-06-05 17:34:56 +0900

    Modify script

--- example/cpu_usage_check.py
+++ example/cpu_usage_check.py
@@ -9,22 +9,29 @@ from zense_pywrapper import PyPicoZenseManager
 import pdb
 import psutil
 from enum import Enum, IntEnum
+from datetime import datetime
+import numpy as np
+
+
+def get_timestamp_milliseconds():
+    return float((datetime.now() - datetime.utcfromtimestamp(0)).total_seconds() * 1e3)
 
 
 # Unique objects
 zense_mng = PyPicoZenseManager(0, debug=True)
-this_pid = filter(lambda p: psutil.Process(
-    p).name() == "python", psutil.pids())[0]
+this_pid = filter(
+    lambda p: psutil.Process(p).name() == "python", psutil.pids()
+)[0]
 
 
 class CameraFlags(IntEnum):
-    EnableDepthDistCorrection = 1,
-    EnableIRDistCorrection = 2,
-    EnableRGBDistCorrection = 3,
-    EnableComputeRealDepthFilter = 4,
-    EnableSmoothingFilter = 5,
-    EnabledRGBToDepth = 6,
-    EnabledDepth2RGB = 7
+    EnableDepthDistCorrection = 0,
+    EnableIRDistCorrection = 1,
+    EnableRGBDistCorrection = 2,
+    EnableComputeRealDepthFilter = 3,
+    EnableSmoothingFilter = 4,
+    EnabledRGBToDepth = 5,
+    EnabledDepth2RGB = 6
 
 
 class Setting:
@@ -81,25 +88,34 @@ def configure_flipping(switch_flag):
 
 
 def monitor(switch_flags):
-    zense_setting = Setting(switch_flags)
+    zense_setting = Setting()
     set_setting(zense_mng, zense_setting)
-    process = psutil.Process(this_pid)
-
     # log cpu usage of `worker_process` every 10 ms
     cpu_percents = []
     count = 1
-    while count < 1000:
-        zense_mng.update()
+    start_time = get_timestamp_milliseconds()
+    while count < 500:
         if (count % 100) == 0:
-            configure_flipping()
-        cpu_percents.append(process.cpu_percent())
+            configure_flipping(switch_flags)
+        cpu_percents.append(
+            [(get_timestamp_milliseconds()-start_time)/1000, psutil.cpu_percent()])
         print("{}%".format(cpu_percents[-1]))
         time.sleep(0.05)
         count += 1
-
     return cpu_percents
 
 
-zense_setting = Setting()
-zense_setting.flip(CameraFlags.EnableComputeRealDepthFilter)
-cpu_percents = monitor()
+cpu_percents_depth_dist = monitor(CameraFlags.EnableDepthDistCorrection)
+np.savetxt("data/cpu_percents_depth_dist.txt" , cpu_percents_depth_dist)
+cpu_percents_ir_dist = monitor(CameraFlags.EnableIRDistCorrection)
+np.savetxt("data/cpu_percents_ir_dist.txt" , cpu_percents_ir_dist)
+cpu_percents_rgb_dist = monitor(CameraFlags.EnableRGBDistCorrection)
+np.savetxt("data/cpu_percents_rgb_dist.txt" , cpu_percents_rgb_dist)
+cpu_percents_real_depth = monitor(CameraFlags.EnableComputeRealDepthFilter)
+np.savetxt("data/cpu_percents_real_depth.txt" , cpu_percents_real_depth)
+cpu_percents_smooth_filter = monitor(CameraFlags.EnableSmoothingFilter)
+np.savetxt("data/cpu_percents_smooth_filter.txt" , cpu_percents_smooth_filter)
+cpu_percents_enable_rgb_to_depth = monitor(CameraFlags.EnabledRGBToDepth)
+np.savetxt("data/cpu_percents_enable_rgb_to_depth.txt" , cpu_percents_enable_rgb_to_depth)
+cpu_percents_enable_depth_to_rgb = monitor(CameraFlags.EnabledDepth2RGB)
+np.savetxt("data/cpu_percents_enable_depth_to_rgb.txt" , cpu_percents_enable_depth_to_rgb)
